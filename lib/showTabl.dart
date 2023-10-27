0b8699ea16c70bd53df90be16943173639607d69
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-10-27 13:55:27 +0200

    created a showTablå file with object

---
+++ lib/showTabl.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class showTabl extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        height: 200,
+        color: Colors.black,
+      ),
+    );
+  }
+}
